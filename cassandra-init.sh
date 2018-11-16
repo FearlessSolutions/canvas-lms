219344f370a2138fb621a1382d4dbd7981a1d599
TJ Famodu <tfamodu@gmail.com>
2018-11-16 12:11:30 +0000

    Removed DROP TABLES from cassandra init script

--- cassandra-init.sh
+++ cassandra-init.sh
@@ -1,18 +1,10 @@
 cat >/import.cql <<EOF
-DROP keyspace page_views;
-DROP keyspace page_views_test;
-DROP keyspace auditors;
-DROP keyspace auditors_test;
-CREATE keyspace page_views with replication = {'class':'SimpleStrategy', 'replication_factor' : 1};
-CREATE keyspace page_views_test with replication = {'class':'SimpleStrategy', 'replication_factor' : 1};
-CREATE keyspace auditors with replication = {'class':'SimpleStrategy', 'replication_factor' : 1};
-CREATE keyspace auditors_test with replication = {'class':'SimpleStrategy', 'replication_factor' : 1};
+CREATE keyspace IF NOT EXISTS page_views with replication = {'class':'SimpleStrategy', 'replication_factor' : 1};
+CREATE keyspace IF NOT EXISTS page_views_test with replication = {'class':'SimpleStrategy', 'replication_factor' : 1};
+CREATE keyspace IF NOT EXISTS auditors with replication = {'class':'SimpleStrategy', 'replication_factor' : 1};
+CREATE keyspace IF NOT EXISTS auditors_test with replication = {'class':'SimpleStrategy', 'replication_factor' : 1};
 EOF
 
-# You may add some other conditionals that fits your stuation here
-until cqlsh -f /import.cql; do
-  echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
-  sleep 2
-done &
+cqlsh -f /import.cql; 
 
-exec /docker-entrypoint.sh "$@"
+
